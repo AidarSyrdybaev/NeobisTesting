130984e9e25b794d3bb779a272e67801673deee1
aidar aidar <aidarsyrdybaev1998@gmail.com>
2020-06-09 15:35:54 +0600

    DeadLine2

---
+++ Tasks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var PriceList = new float[] { 4, 4.5f, 5, 2, 1.5f };
+            var NumbersTxt = Console.ReadLine().Split(' ');
+            var Sum = PriceList[int.Parse(NumbersTxt[0]) - 1] * int.Parse(NumbersTxt[1]);
+            Console.WriteLine($"Total: R$ {string.Format("{0:F2}", Sum)}");
+        }
+    }
+}
